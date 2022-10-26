ad03b597dd8ac6c7bd1a6f3a90d352a7b8cb92d9
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2022-10-26 16:48:09 +0330

    complete validation

--- src/Components/Sign-in/Sign-in.jsx
+++ src/Components/Sign-in/Sign-in.jsx
@@ -1,4 +1,5 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import validate from '../../utils/validate'
 import "./Sign_in.css";
 function Sign_In() {
     const [data, setData] = useState({
@@ -8,33 +9,51 @@ function Sign_In() {
         confirm: "",
         isAccepted: false
     })
-    const handler = (e) => {
-        if (e.target.name !== "isAccepted") {
-            setData({...data,[e.target.name]: e.target.value})
+    const [errorrs, setErrors] = useState({});
+    const [touched, setTouched] = useState({});
+
+    useEffect(() => {
+        setErrors(validate(data));
+    },[data])
+
+    const handler = event => {
+        if (event.target.name !== "isAccepted") {
+            setData({...data,[event.target.name]: event.target.value})
         }
         else{
-            setData({...data,[e.target.name]: e.target.checked})
+            setData({...data,[event.target.name]: event.target.checked})
         }
+    }
+    const focusHandler = event => {
+        setTouched({
+            ...touched,
+            [event.target.name]: true,
+        })
     }
     return ( 
         <form className="Sign_in">
             <h1 className="Topic">Sign-in</h1>
             <div>
                 {/* <label>Name</label> */}
-                <input name="name" className="text_Input" type="text" onChange={handler} placeholder="Name"/>
+                <input name="name" className="text_Input" type="text" onChange={handler} onFocus={focusHandler} placeholder="Name"/>
+                {errorrs.name && touched.name && <span className='error'>{errorrs.name}</span>}
             </div>
             <div>
-                <input name="email" className="text_Input" type="text" onChange={handler} placeholder="Email"/>
+                <input name="email" className="text_Input" type="text" onChange={handler} onFocus={focusHandler} placeholder="Email"/>
+                {errorrs.email && touched.email && <span className='error'>{errorrs.email}</span>}
             </div>
             <div>
-                <input name="password" className="text_Input" type="text" onChange={handler} placeholder="Password"/>
+                <input name="password" className="text_Input" type="text" onChange={handler} onFocus={focusHandler} placeholder="Password"/>
+                {errorrs.password && touched.password &&  <span className='error'>{errorrs.password}</span>}
             </div>
             <div>
-                <input name="confirm" className="text_Input" type="text" onChange={handler} placeholder="Password Confirm"/>
+                <input name="confirm" className="text_Input" type="text" onChange={handler} onFocus={focusHandler} placeholder="Password Confirm"/>
+                {errorrs.confirm && touched.confirm &&  <span className='error'>{errorrs.confirm}</span>}
             </div>
             <div className="checkbox">
                 <label htmlFor="accept-id"> <b>I accept terms of privacy policy</b> </label>
-                <input id="accept-id" name="isAccepted" className="accept" type="checkbox" onChange={handler}/>
+                <input id="accept-id" name="isAccepted" className="accept" type="checkbox" onChange={handler} onFocus={focusHandler}/>
+                {errorrs.isAccepted && touched.isAccepted &&  <span className='error'>{errorrs.isAccepted}</span>}
             </div>
             <div>
                 <input className="submit_btn" type="submit" />
---
+++ src/utils/validate.js
@@ -0,0 +1,38 @@
+const validate = (data) => {
+    const error = {};
+
+    // validate of name :
+    if (!data.name.trim()) {
+        error.name = "name requirement !";
+    }
+    else{delete error.name};
+
+    // validate of email :
+    if (!data.email) {
+        error.email = "email requirement !"
+    }else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email)) {
+        error.email = "invalid email"
+    }else{delete error.email};
+    
+    // validate of pass :
+    if (!data.password) {
+        error.password = "password requirement !"
+    }else if(!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(data.password)){
+        error.password = "Minimum eight char at least a letter and a number"
+    }else{delete error.password};
+
+    // validate of confirm :
+    if (!data.confirm) {
+        error.confirm = "plz confirm password"
+    }else if (data.confirm !== data.password) {
+        error.confirm = "password isn't match"
+    }else{delete error.confirm};
+
+    // validate of privacy :
+    if (!data.isAccepted) {
+       error.isAccepted = "plz accept our policy"; 
+    }else{delete error.isAccepted};
+
+    return error;
+}
+export default validate;
